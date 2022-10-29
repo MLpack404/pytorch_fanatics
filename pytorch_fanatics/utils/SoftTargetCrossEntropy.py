b32fdbba41d022e91bc306424d41be0544565297
MiHarsh <harshm17172612@gmail.com>
2022-10-29 10:06:31 +0530

    code push

---
+++ pytorch_fanatics/utils/SoftTargetCrossEntropy.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class SoftTargetCrossEntropy(nn.Module):
+
+    def __init__(self):
+        super(SoftTargetCrossEntropy, self).__init__()
+
+    def forward(self, x, target):
+        loss = torch.sum(-target * F.log_softmax(x, dim=-1), dim=-1)
+        return loss.mean()
